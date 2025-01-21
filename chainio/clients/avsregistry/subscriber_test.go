500af3ceb014899d5cbea3e6b5a066622b1f56f6
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-01-21 17:28:45 -0300

    Move defer after error check

--- chainio/clients/avsregistry/subscriber_test.go
+++ chainio/clients/avsregistry/subscriber_test.go
@@ -21,15 +21,15 @@ func TestSubscriberAvsRegistry(t *testing.T) {
 
 	t.Run("subscribe to new pubkey registrations", func(t *testing.T) {
 		pubKeyRegistrationsC, event, err := chainSubscriber.SubscribeToNewPubkeyRegistrations()
-		defer event.Unsubscribe()
 		require.NoError(t, err)
+		defer event.Unsubscribe()
 
 		// Emit a NewPubkeyRegistration event creating a new operator
 		keypair, err := bls.NewKeyPairFromString("0x01")
-		require.NoError(t, err, "Failed to create BLS key pair")
+		require.NoError(t, err)
 
 		ecdsaPrivateKey, err := crypto.HexToECDSA(testutils.ANVIL_FIRST_PRIVATE_KEY)
-		require.NoError(t, err, "Failed to parse ECDSA private key")
+		require.NoError(t, err)
 
 		quorumNumbers := types.QuorumNums{0}
 
@@ -56,8 +56,8 @@ func TestSubscriberAvsRegistry(t *testing.T) {
 
 	t.Run("subscribe to operator socket updates", func(t *testing.T) {
 		socketC, event, err := chainSubscriber.SubscribeToOperatorSocketUpdates()
-		defer event.Unsubscribe()
 		require.NoError(t, err)
+		defer event.Unsubscribe()
 
 		// Emit a SocketUpdate event
 		socketUpdate := "socket-update"
@@ -70,7 +70,7 @@ func TestSubscriberAvsRegistry(t *testing.T) {
 			assert.Equal(t, socketUpdate, operatorSocketUpdate.Socket)
 		case <-time.After(10 * time.Second):
 			// Throw an error if the event is not received within 10 seconds, making the test fail
-			t.Fatal("Timed out waiting for NewPubkeyRegistration event")
+			t.Fatal("Timed out waiting for operatorSocketUpdate event")
 		}
 	})
 }
