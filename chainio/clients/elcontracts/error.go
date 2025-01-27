08311e15ca906fafdd3b7b9cc808a401e6c76f75
maximopalopoli <mpalopoli@fi.uba.ar>
2025-01-27 17:57:44 -0300

    Create error generic struct

---
+++ chainio/clients/elcontracts/error.go
@@ -0,0 +1,19 @@
+package elcontracts
+
+import "fmt"
+
+type Error struct {
+	code        int
+	message     string
+	description string
+	cause       error
+	// metadata    map[string]interface{}
+}
+
+func (e Error) Error() string {
+	if e.cause != nil {
+		return fmt.Sprintf("%s(%d) - %s: %s", e.message, e.code, e.description, e.cause.Error())
+	} else {
+		return fmt.Sprintf("%s(%d) - %s", e.message, e.code, e.description)
+	}
+}
