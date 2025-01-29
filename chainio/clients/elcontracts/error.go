f97103491e9fccde4c354cc1e0ed46450fc657a4
Maximo Palopoli <96491141+maximopalopoli@users.noreply.github.com>
2025-01-29 10:04:20 -0300

    Update chainio/clients/elcontracts/error.go
    Co-authored-by: Damian Ramirez <daramirez@frba.utn.edu.ar>

--- chainio/clients/elcontracts/error.go
+++ chainio/clients/elcontracts/error.go
@@ -13,9 +13,8 @@ type Error struct {
 func (e Error) Error() string {
 	if e.cause != nil {
 		return fmt.Sprintf("%s(%d) - %s: %s", e.message, e.code, e.description, e.cause.Error())
-	} else {
-		return fmt.Sprintf("%s(%d) - %s", e.message, e.code, e.description)
 	}
+	return fmt.Sprintf("%s(%d) - %s", e.message, e.code, e.description)
 }
 
 func CreateErrorForMissingContract(contractName string) Error {
