7000819b9c3ce66264bf932ef27ae602124473d1
maximopalopoli <mpalopoli@fi.uba.ar>
2025-01-30 18:00:23 -0300

    Change ecdsa package interface

--- crypto/ecdsa/utils.go
+++ crypto/ecdsa/utils.go
@@ -9,14 +9,77 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common"
-	gethcommon "github.com/ethereum/go-ethereum/common"
-
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/google/uuid"
 )
 
+type ECDSAKey struct {
+	key *ecdsa.PrivateKey
+}
+
+// Save encrypts and stores the private key at the given path
+func (e ECDSAKey) Save(path string, password string) error {
+	UUID, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+
+	// We are using https://github.com/ethereum/go-ethereum/blob/master/accounts/keystore/key.go#L41
+	// to store the keys which requires us to have random UUID for encryption
+	key := &keystore.Key{
+		Id:         UUID,
+		Address:    crypto.PubkeyToAddress(e.key.PublicKey),
+		PrivateKey: e.key,
+	}
+
+	encryptedBytes, err := keystore.EncryptKey(key, password, keystore.StandardScryptN, keystore.StandardScryptP)
+	if err != nil {
+		return err
+	}
+
+	return writeBytesToFile(path, encryptedBytes)
+}
+
+// Read loads an encrypted private key from the given path
+func (e *ECDSAKey) Read(path string, password string) (ECDSAKey, error) {
+	keyStoreContents, err := os.ReadFile(filepath.Clean(path))
+	if err != nil {
+		return ECDSAKey{}, err
+	}
+
+	sk, err := keystore.DecryptKey(keyStoreContents, password)
+	if err != nil {
+		return ECDSAKey{}, err
+	}
+
+	return ECDSAKey{key: sk.PrivateKey}, nil
+}
+
+// CreateNewEcdsaKey generates a new ECDSA private key
+func CreateNewEcdsaKey() (ECDSAKey, error) {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		return ECDSAKey{}, err
+	}
+	return ECDSAKey{key: privateKey}, nil
+}
+
+// CreateNewEcdsaKey generates a new ECDSA private key
+func CreateNewEcdsaKeyFromHex(privateKeyHex string) (ECDSAKey, error) {
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	if err != nil {
+		return ECDSAKey{}, err
+	}
+	return ECDSAKey{key: privateKey}, nil
+}
+
+func (e ECDSAKey) GetPrivateKey() *ecdsa.PrivateKey {
+	return e.key
+}
+
+/*
 func WriteKeyFromHex(path, privateKeyHex, password string) error {
 	privateKey, err := crypto.HexToECDSA(privateKeyHex)
 	if err != nil {
@@ -51,6 +114,22 @@ func WriteKey(path string, privateKey *ecdsa.PrivateKey, password string) error 
 	return writeBytesToFile(path, encryptedBytes)
 }
 
+func ReadKey(keyStoreFile string, password string) (*ecdsa.PrivateKey, error) {
+	keyStoreContents, err := os.ReadFile(filepath.Clean(keyStoreFile))
+	if err != nil {
+		return nil, err
+	}
+
+	sk, err := keystore.DecryptKey(keyStoreContents, password)
+	if err != nil {
+		return nil, err
+	}
+
+	return sk.PrivateKey, nil
+}
+
+*/
+
 func writeBytesToFile(path string, data []byte) error {
 	dir := filepath.Dir(path)
 
@@ -80,39 +159,25 @@ func writeBytesToFile(path string, data []byte) error {
 	return err
 }
 
-func ReadKey(keyStoreFile string, password string) (*ecdsa.PrivateKey, error) {
-	keyStoreContents, err := os.ReadFile(filepath.Clean(keyStoreFile))
-	if err != nil {
-		return nil, err
-	}
-
-	sk, err := keystore.DecryptKey(keyStoreContents, password)
-	if err != nil {
-		return nil, err
-	}
-
-	return sk.PrivateKey, nil
-}
-
 // GetAddressFromKeyStoreFile We are using Web3 format defined by
 // https://ethereum.org/en/developers/docs/data-structures-and-encoding/web3-secret-storage/
-func GetAddressFromKeyStoreFile(keyStoreFile string) (gethcommon.Address, error) {
+func GetAddressFromKeyStoreFile(keyStoreFile string) (common.Address, error) {
 	keyJson, err := os.ReadFile(filepath.Clean(keyStoreFile))
 	if err != nil {
-		return gethcommon.Address{}, err
+		return common.Address{}, err
 	}
 
 	// The reason we have map[string]interface{} is because `address` is string but the `crypto` field is an object
 	// we don't care about the object in this method, but we still need to unmarshal it
 	m := make(map[string]interface{})
 	if err := json.Unmarshal(keyJson, &m); err != nil {
-		return gethcommon.Address{}, err
+		return common.Address{}, err
 	}
 
 	if address, ok := m["address"].(string); !ok {
-		return gethcommon.Address{}, fmt.Errorf("address not found in key file")
+		return common.Address{}, fmt.Errorf("address not found in key file")
 	} else {
-		return gethcommon.HexToAddress(address), nil
+		return common.HexToAddress(address), nil
 	}
 }
 
