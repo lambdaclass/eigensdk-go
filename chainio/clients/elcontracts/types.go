a3eedfb3246a97759e1d1b800290ff3f86646ae7
Damian Ramirez <damian.ramirez@lambdaclass.com>
2025-02-03 10:00:22 -0300

    Create new req-res structs for reader

--- chainio/clients/elcontracts/types.go
+++ chainio/clients/elcontracts/types.go
@@ -81,3 +81,81 @@ type RemovePendingAdminRequest struct {
 	AdminAddress   common.Address
 	WaitForReceipt bool
 }
+
+// IsAdminRequest represents a request to check if the caller is an admin of an account
+type AdminCheckRequest struct {
+	BlockNumber    *big.Int
+	AccountAddress common.Address
+	AdminAddress   common.Address
+}
+
+// IsAdminResponse shows whether an address is an admin.
+type IsAdminResponse struct {
+	IsAdmin bool
+}
+
+// IsPendingAdminRequest represents a request to check if an address is a pending admin of the account
+type PendingAdminCheckRequest struct {
+	BlockNumber         *big.Int
+	AccountAddress      common.Address
+	PendingAdminAddress common.Address
+}
+
+// IsPendingAdminResponse shows whether an address is a pending admin.
+type IsPendingAdminResponse struct {
+	IsPendingAdmin bool
+}
+
+// AccountRequest represents the address of an specific account
+type AccountRequest struct {
+	BlockNumber    *big.Int
+	AccountAddress common.Address
+}
+
+// AdminsResponse shows the list of admins of an account
+type AdminsResponse struct {
+	Admins []common.Address
+}
+
+// PendingAdminsResponse shows the list of pending admins of an account
+type PendingAdminsResponse struct {
+	PendingAdmins []common.Address
+}
+
+// AppointeesListRequest represents a request to get the list of appointees of an account
+type AppointeesListRequest struct {
+	BlockNumber    *big.Int
+	AccountAddress common.Address
+	TargetAddress  common.Address
+	Selector       [4]byte
+}
+
+// AppointeesResponse shows the list of appointees of an account
+type AppointeesResponse struct {
+	Appointees []common.Address
+}
+
+type AppointeePermissionsListRequest struct {
+	BlockNumber      *big.Int
+	AccountAddress   common.Address
+	AppointeeAddress common.Address
+}
+
+type AppointeePermissionsListResponse struct {
+	TargetAddresses []common.Address
+	Selectors       [][4]byte
+}
+
+// CanCallRequest represents a request to check if an account can call a target
+type CanCallRequest struct {
+	BlockNumber      *big.Int
+	AccountAddress   common.Address
+	AppointeeAddress common.Address
+	Target           common.Address
+	Selector         [4]byte
+}
+
+// CanCallResponse shows whether an account can call a target
+type CanCallResponse struct {
+	CanCall bool
+}
